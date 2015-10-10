b46712cd455af1f78a9d12b2fb87486342457c2b
AceMood <zmike86@163.com>
2015-10-10 22:54:17 +0800

    dev

--- __code/Haste.js
+++ __code/Haste.js
@@ -1,7 +1,18 @@
 /**
- * Created by acemood on 15-10-10.
+ * Copyright 2013 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('util').inherits;
 var loaders = require('./loaders');
@@ -221,7 +232,7 @@ Haste.prototype.watch = function(path, callback, options) {
  */
 Haste.prototype.updateMap = function(map, callback) {
   this.getFinder().find(function(files) {
-    this.createUpdateTask(files, map).on('complete', callback).run();
+     this.createUpdateTask(files, map).on('complete', callback).run();
   }.bind(this));
 };
 
@@ -327,8 +338,8 @@ Haste.prototype.getFinder = function() {
  */
 Haste.prototype.getSerializer = function() {
   return this.serializer || new ResourceMapSerializer(
-      this.loaders,
-      { version: this.options.version });
+    this.loaders,
+    { version: this.options.version });
 };
 
 module.exports = Haste;
