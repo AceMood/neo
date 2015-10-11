5f582a6fce27f07567bccda3293d48583259a42a
AceMood <zmike86@163.com>
2015-10-11 16:41:43 +0800

    dev

--- __code/FileFinder.js
+++ __code/FileFinder.js
@@ -1,52 +1,37 @@
-/**
- * Copyright 2013 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 var fs = require('fs');
 var path = require('path');
 
 /**
- * Scans directories for files with given extensions (async)
- * Will not follow symlinks. Uses node.js native function to traverse, can
- * be slower, but more safe than findNative
+ * 按照给定的扩展名扫描目录（异步操作）。
+ * Will not follow symlinks. 用node.js原生的方法去遍历会慢一些但是比用findNative安全些。
  *
- * @param  {Array.<String>} scanDirs   Directories to scan, ex: ['html/']
- * @param  {Array.<String>} extensions Extensions to searc for, ex: ['.js']
+ * @param  {Array.<String>} scanDirs   要扫描的目录, ex: ['html/']
+ * @param  {Array.<String>} extensions 文件扩展名, ex: ['.js']
  * @param  {function|null}  ignore     Optional function to filter out paths
- * @param  {Function}       callback
+ * @param  {Function}       callback   回调函数
  */
-function find(scanDirs, extensions, ignore, callback) {
+function find (scanDirs, extensions, ignore, callback) {
   var result = [];
   var activeCalls = 0;
 
-  function readdirRecursive(curDir) {
+  function readdirRecursive (curDir) {
     activeCalls++;
-    fs.readdir(curDir, function(err, names) {
+    fs.readdir(curDir, function (err, names) {
       activeCalls--;
 
+      // 标准化文件路径
       for (var i = 0; i < names.length; i++) {
         names[i] = path.join(curDir, names[i]);
       }
 
-      names.forEach(function(curFile) {
+      names.forEach(function (curFile) {
         if (ignore && ignore(curFile)) {
           return;
         }
         activeCalls++;
 
-        fs.lstat(curFile, function(err, stat) {
+        fs.lstat(curFile, function (err, stat) {
           activeCalls--;
 
           if (!err && stat && !stat.isSymbolicLink()) {
@@ -54,6 +39,7 @@ function find(scanDirs, extensions, ignore, callback) {
               readdirRecursive(curFile);
             } else {
               var ext = path.extname(curFile);
+              // 扩展名符合文件类型
               if (extensions.indexOf(ext) !== -1) {
                 result.push([curFile, stat.mtime.getTime()]);
               }
@@ -80,20 +66,21 @@ function find(scanDirs, extensions, ignore, callback) {
  * node.js based implementation though as any shell command is suspectable to
  * attacks. Use with caution.
  *
- * @param  {Array.<String>} scanDirs   Directories to scan, ex: ['html/']
- * @param  {Array.<String>} extensions Extensions to searc for, ex: ['.js']
+ * @param  {Array.<String>} scanDirs   要扫描的目录, ex: ['html/']
+ * @param  {Array.<String>} extensions 文件扩展名, ex: ['.js']
  * @param  {function|null}  ignore     Optional function to filter out paths
- * @param  {Function}       callback
+ * @param  {Function}       callback   回调函数
  */
-function findNative(scanDirs, extensions, ignore, callback) {
+function findNative (scanDirs, extensions, ignore, callback) {
   var os = require('os');
-  if(os.platform() == 'win32'){
-    return find(scanDirs,extensions,ignore,callback);
+  if (os.platform() == 'win32') {
+    return find(scanDirs, extensions, ignore, callback);
   }
+
   var spawn = require('child_process').spawn;
   var args = [].concat(scanDirs);
   args.push('-type', 'f');
-  extensions.forEach(function(ext, index) {
+  extensions.forEach(function (ext, index) {
     if (index) {
       args.push('-o');
     }
@@ -104,22 +91,22 @@ function findNative(scanDirs, extensions, ignore, callback) {
   var findProcess = spawn('find', args);
   var stdout = '';
   findProcess.stdout.setEncoding('utf-8');
-  findProcess.stdout.on('data', function(data) {
+  findProcess.stdout.on('data', function (data) {
     stdout += data;
   });
 
-  findProcess.stdout.on('close', function(code) {
+  findProcess.stdout.on('close', function (code) {
     // Split by lines, trimming the trailing newline
     var lines = stdout.trim().split('\n');
     if (ignore) {
-      var include = function(x) {
+      var include = function (x) {
         return !ignore(x);
       };
       lines = lines.filter(include);
     }
     var result = [];
     var count = lines.length;
-    // for (var i = 0; i < count; i++){
+    // for (var i = 0; i < count; i++) {
     //   if (lines[i]) {
     //     var stat = fs.statSync(lines[i]);
     //     if (stat) {
@@ -128,8 +115,8 @@ function findNative(scanDirs, extensions, ignore, callback) {
     //   }
     // }
     // callback(result);
-    lines.forEach(function(path) {
-      fs.stat(path, function(err, stat) {
+    lines.forEach(function (path) {
+      fs.stat(path, function (err, stat) {
         if (stat && !stat.isDirectory()) {
           result.push([path, stat.mtime.getTime()]);
         }
@@ -146,7 +133,7 @@ function findNative(scanDirs, extensions, ignore, callback) {
  * @class
  * @param {Object} options
  */
-function FileFinder(options) {
+function FileFinder (options) {
   this.scanDirs = options && options.scanDirs || ['.'];
   this.extensions = options && options.extensions || ['.js'];
   this.ignore = options && options.ignore || null;
@@ -156,7 +143,7 @@ function FileFinder(options) {
 /**
  * @param  {Function} callback
  */
-FileFinder.prototype.find = function(callback) {
+FileFinder.prototype.find = function (callback) {
   var impl = this.useNative ? findNative : find;
   impl(this.scanDirs, this.extensions, this.ignore, callback);
 };
--- __code/Haste.js
+++ __code/Haste.js
@@ -1,18 +1,4 @@
-/**
- * Copyright 2013 Facebook, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 var EventEmitter = require('events').EventEmitter;
 var inherits = require('util').inherits;
 var loaders = require('./loaders');
@@ -141,7 +127,7 @@ Haste.prototype.update = function(path, callback, options) {
         callback(map, task.messages);
       }
     }).run();
-  }
+  };
 
   this.getFinder().find(function(f) {
     files = f;
