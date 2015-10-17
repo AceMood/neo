7f266da1b3dab70f128025797a3e016972cacd9f
AceMood <zmike86@163.com>
2015-10-17 22:02:50 +0800

    dev

--- sample/start.js
+++ sample/start.js
@@ -1,10 +1,13 @@
 
 var Neo = require('../lib/Neo');
+var loaders = Neo.loaders;
 
+// 第一个数组表示需要针对何种类型的资源
+// 第二个数组表示需要遍历的目录，应该是相对于vrd的路径
 var neo = new Neo(
   [
-    new JSLoader(),
-    new CSSLoader()
+    new loaders.JSLoader(),
+    new loaders.CSSLoader()
   ],
   [
     "static",
@@ -13,5 +16,6 @@ var neo = new Neo(
 );
 
 neo.update('map.json', function(map) {
-
+  // 根据返回的map对象进行业务框架定制
+  //
 });
