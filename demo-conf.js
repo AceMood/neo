bb7884342e3c7c8d43f445e80e76e0bf46bf05f6
AceMood <zmike86@163.com>
2015-12-09 15:59:08 +0800

    test

--- demo-conf.js
+++ demo-conf.js
@@ -2,14 +2,17 @@
  * Created by baidu on 15/11/26.
  * @sample
  * plugin list:
- * 1. csscompress (built-in)
- * 2. less (built-in)
- * 3. uglify (built-in)
- * 4. md5 (built-in)
- * 5. replacer (built-in)
- * 6. packager
- * 7. tpl-loader (3rd)
+ * 1. csscompress
+ * 2. less
+ * 3. uglify
+ * 4. replacer
+ * 5. md5
  */
+
+// 配置线上路径
+soi
+    .addRule(/merchant\/img\/.*\.png$/).to('static/images/')
+    .addRule(/merchant\/(.*)\/.*\.js$/).to('static/js/');
 
 soi.deploy.task('dev',
     {
@@ -19,20 +22,14 @@ soi.deploy.task('dev',
       scandirs: ['.']
     })
     .use('replacer', {
-      from: '__NAVBAR__',
-      to: function($0, $1) {
+      '__NAVBAR__': function($0, $1) {
         if ($0 === '__NAVBAR__') {
           return 'zhida.baidu.com'
         }
       }
     })
-    .addRule(/(.*)\/(.*)\.less/)
-      .use('less')
-    .to('static/merchant/$1/$2.css')
-    .addRule(/(.*)\/(.*)\.js/)
-    .to('static/merchant/$1/$2.css')
-    .addRule('*.tpl')
-    .use('plugin-tplloader', {
+    .use('less')
+    .postProcess('plugin-tplloader', {
       left: '{{',
       right: '}}'
     });
@@ -44,7 +41,8 @@ soi.release.task('dev',
       mapTo: '../build/map.json',
       cacheTo: '../build/.cache',
       scandirs: ['.'],
-      wrapper: {
+      ignorePaths: noop,
+      cmdWrapper: {
         define: '__d',
         usestrict: false,
         commentdoc: ''
@@ -52,44 +50,27 @@ soi.release.task('dev',
     })
     // replacer操作
     .use('replacer', {
-      from: '__NAVBAR__',
-      to: function($0, $1) {
+      '__NAVBAR__': function($0, $1) {
         if ($0 === '__NAVBAR__') {
           return 'zhida.baidu.com'
         }
       }
     })
-    .addRule(/merchant\/img\/.*\.png$/)
-      .use('md5')
-      .to('static/images/')
-    .addRule(/(.*)\.less$/)
-      .use('less')
-      .use('css')
-      .use('md5')
-    .addRule('*.js')
-      .use('uglify', {
-        debug: false,
-        curl: true,
-        eqeqeq: false
-      })
-      .use('md5', {
-        length: 9,
-        noname: true,
-        encoding: 'base64'
-      })
-      .to('static/js/')
-    .addRule(/merchant\/(.*)\/.*\.js$/)
-      .use('packager', {
-        'static/js/base.js': [
-          'base/*.js'
-        ],
-        'static/js/lib.js': [
-          'lib/*.js'
-        ]
-      })
-    .addRule('*.tpl')
-      .use('plugin-tplloader', {
-        left: '{{',
-        right: '}}'
-      });
+    .use('less')
+    .use('css')
+    .use('hash', {
+      algorithm: 'sha1',
+      length: 9,
+      noname: true,
+      encoding: 'base64'
+    })
+    .use('uglify', {
+      debug: false,
+      curl: true,
+      eqeqeq: false
+    })
+    .postProcess('plugin-tplloader', {
+      left: '{{',
+      right: '}}'
+    });
 
