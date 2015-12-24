c770fdb93bcbc34ec364d1379a2ae12339572389
AceMood <zmike86@163.com>
2015-12-24 14:47:21 +0800

    fix require path

--- sample/color.js
+++
@@ -1,9 +0,0 @@
-var cli = require('../lib/CLI');
-
-console.log(cli.color('green', 'Hello, Green Text!'));
-console.log(cli.underline('Hello, underlined Text!'));
-console.log(cli.bold('Hello, bold Text!'));
-console.log(cli.awesome('Hello, awesome Text!'));
-console.log(cli.color('magenta', 'Hello, magenta Text!'));
-console.log(cli.color('blue', 'Hello, blue Text!'));
-console.log(cli.color('yellow', 'Hello, yellow Text!'));
--- sample/nextTick.js
+++
@@ -1,3 +0,0 @@
-for (var i = 0; i < 1024 * 1024; i++) {
-    process.nextTick(function () { Math.sqrt(i) } )
-}
