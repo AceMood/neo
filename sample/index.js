24f2af113ae74c6d17ccad229f1daf60bc8470cf
AceMood <zmike86@163.com>
2016-01-07 14:48:20 +0800

    dev

--- sample/index.js
+++ sample/index.js
@@ -15,30 +15,27 @@ var Loaders = Neo.Loaders;
 // 第一个数组表示需要针对何种类型的资源
 // 第二个数组表示需要遍历的目录，应该是相对于vrd的路径
 // 第三个是配置对象
-var neo = new Neo(
-    [
-      new Loaders.JSLoader()
-      //new Loaders.CSSLoader(),
-      //new Loaders.ImageLoader(),
-      //new Loaders.SWFLoader()
-    ],
-    [
-      /*'core',*/ 'base', 'project'
-    ]
-);
+var neo = new Neo([
+  new Loaders.JSLoader(),
+  new Loaders.CSSLoader(),
+  new Loaders.ImageLoader(),
+  new Loaders.SWFLoader()
+], [
+  /*'core',*/ 'base', 'project'
+]);
 
 neo.on('postProcessed', function(map) {
 
-    // debugger;
+  // debugger;
   // 基础服务组件生效，根据配置进行代码优化打包
 
 });
 
 neo.update('.cache', function(map, messages) {
 
-    debugger;
-    console.log(messages.render());
+  debugger;
+  console.log(messages.render());
 
-    // 根据返回的map对象进行业务框架定制
-    fs.writeFileSync('map.json', JSON.stringify(map, null, 4), 'utf8');
+  // 根据返回的map对象进行业务框架定制
+  fs.writeFileSync('map.json', JSON.stringify(map, null, 4), 'utf8');
 });
