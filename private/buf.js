4d11fb12f0732f7614e49179923471c9baf555bf
AceMood <zmike86@163.com>
2016-03-31 17:15:34 +0800

    dev

--- private/buf.js
+++
@@ -1,9 +0,0 @@
-str = '\u00bd + \u00bc = \u00be';
-var buf = new Buffer(str);
-
-console.log(`${str}: ${str.length} characters, ` +
-  `${Buffer.byteLength(str, 'utf8')} bytes, and buf.length is ${buf.length}`);
-
-str = 'hello';
-console.log(`${str}: ${str.length} characters, ` +
-  `${Buffer.byteLength(str, 'utf8')} bytes, and buf.length is ${buf.length}`);
